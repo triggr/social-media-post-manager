735e025e66718aadfd0c4fea4e3ace87e620aa86
Anthony Vespoli <avespoli@sonarmd.com>
2024-11-08 18:19:00 -0600

    initial checkin

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  /** ADDED **/
+  output: "export", // this is for static site generation for github pages, you can remove if its breaking
 };
 
 export default nextConfig;
